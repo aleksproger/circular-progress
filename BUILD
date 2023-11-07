35d1b076a4342d06ebf0a148ebeb527d4b59576b
Aleksei Sapitskii <sapial@f-secure.com>
2023-11-07 12:06:14 +0200

    Added Bazel support and .rotating style

---
+++ BUILD
@@ -0,0 +1,15 @@
+load("@build_bazel_rules_swift//swift:swift.bzl", "swift_library")
+
+swift_library(
+    name = "CircularProgress",
+    srcs = [
+        "Sources/Examples/Example.swift",
+        "Sources/Examples/Example+OnTimer.swift",
+        "Sources/CircularProgress.swift",
+        "Sources/CircularProgressState.swift",
+        "Sources/CircularProgressStyle.swift",
+        "Sources/DisplayLink+ViewModifier.swift",
+        "Sources/Shapes/Checkmark.swift",
+        "Sources/Shapes/Exclamation.swift",
+    ],
+)
--- Sources/CircularProgressStyle.swift
+++ Sources/CircularProgressStyle.swift
@@ -3,18 +3,103 @@ import SwiftUI
 /// Any custom style must conform to this protocol in order be used in `.progressStyle(_ style:)` view modifier.
 public protocol CircularProgressStyle {
     associatedtype Body: View
-    associatedtype State
+    associatedtype State: RawRepresentable where State.RawValue: BinaryFloatingPoint
     
+    @ViewBuilder
     func makeBody(configuration: CircularProgressStyleConfiguration<State>) -> Body
 }
 
-public struct CircularProgressStyleConfiguration<State> {
+public struct CircularProgressStyleConfiguration<State: RawRepresentable> where State.RawValue: BinaryFloatingPoint {
     let lineWidth: CGFloat
     let state: State
     
-    public init(lineWidth: CGFloat, state: State) {
+    public init(
+        lineWidth: CGFloat,
+        state: State
+    ) {
         self.lineWidth = lineWidth
         self.state = state
+    }
+}
+
+/// Default style which will be applied to `CircularProgress` when no style is applied.
+/// It looks as a simple loader of application's accentColor.
+/// Use the style as `.progressStyle(.simple)` instead of `.progressStyle(DefaultCircularProgressStyle())``
+public struct DefaultCircularProgressStyle: CircularProgressStyle {
+    public func makeBody(configuration: CircularProgressStyleConfiguration<Double>) -> some View {
+        CircularProgress(
+            lineWidth: configuration.lineWidth,
+            state: configuration.state,
+            color: { _ in .accentColor }
+        )
+    }
+}
+
+/// Loader with accentColor color, which shows green checkmark on `.succeeded` and red exclamation on `.failed`
+/// Use the style as `.progressStyle(.interactive)` instead of `.progressStyle(InteractiveCircularProgressStyle())``
+public struct InteractiveCircularProgressStyle: CircularProgressStyle {
+    public func makeBody(configuration: CircularProgressStyleConfiguration<CircularProgressState>) -> some View {
+        CircularProgress(
+            lineWidth: configuration.lineWidth,
+            state: configuration.state,
+            color: { state in
+                switch state {
+                case .inProgress: .blue
+                case .failed: .red
+                case .succeeded: .green }
+            }
+        ) { progressState in
+            switch progressState {
+            case .inProgress: EmptyView()
+            case .failed: ExclamationView(lineWidth: configuration.lineWidth).foregroundColor(.red)
+            case .succeeded: CheckmarkView(lineWidth: configuration.lineWidth).foregroundColor(.green) }
+        }
+    }
+}
+
+/// Loader with accentColor color, which rotates in circle until the progressing is finished.
+/// Use the style as `.progressStyle(.interactive)` instead of `.progressStyle(RotatingProgressStyle())``
+public struct RotatingProgressStyle: CircularProgressStyle {
+    public func makeBody(configuration: CircularProgressStyleConfiguration<Double>) -> some View {
+        RotatingCircleProgress(lineWidth: configuration.lineWidth, state: configuration.state)
+    }
+}
+
+/// Wrapper view needed to store rotation states.
+///  Instance of this view returned by `RotatingProgressStyle`
+struct RotatingCircleProgress<State: RawRepresentable>: View where State.RawValue: BinaryFloatingPoint {
+    @SwiftUI.State
+    private var angle: Double = 0
+    
+    @SwiftUI.State
+    private var fraction: ClosedRange<State.RawValue> = 0...0.25
+    
+    let lineWidth: CGFloat
+    let state: State
+    let speed: Double
+    
+    init(
+        lineWidth: CGFloat,
+        state: State,
+        speed: Double = 0.02
+    ) {
+        self.lineWidth = lineWidth
+        self.state = state
+        self.speed = speed
+    }
+        
+    var body: some View {
+        CircularProgress(
+            lineWidth: lineWidth,
+            state: state,
+            fraction: fraction,
+            color: { _ in .accentColor }
+        )
+        .rotationEffect(.radians(angle))
+        .onDisplayLink { 
+            angle += speed
+            if state.rawValue >= 0.25 { fraction = 0...state.rawValue }
+        }
     }
 }
 
@@ -37,37 +122,7 @@ extension CircularProgressStyle where Self == DefaultCircularProgressStyle {
     public static var simple: DefaultCircularProgressStyle { DefaultCircularProgressStyle() }
 }
 
-/// Default style which will be applied to `CircularProgress` when no style is applied.
-/// It looks as a simple loader of application's accentColor.
-/// Use the style as `.progressStyle(.simple)` instead of `.progressStyle(DefaultCircularProgressStyle())``
-public struct DefaultCircularProgressStyle: CircularProgressStyle {
-    public func makeBody(configuration: CircularProgressStyleConfiguration<Double>) -> some View {
-        CircularProgress(
-            lineWidth: configuration.lineWidth,
-            state: configuration.state,
-            color: { _ in .accentColor }
-        )
-    }
-}
-
-/// Loader with accentColor color, Whihc shows green checkmark on `.succeeded` and red exclamation on `.failed`
-/// Use the style as `.progressStyle(.interactive)` instead of `.progressStyle(InteractiveCircularProgressStyle())``
-public struct InteractiveCircularProgressStyle: CircularProgressStyle {
-    public func makeBody(configuration: CircularProgressStyleConfiguration<CircularProgressState>) -> some View {
-        CircularProgress(
-            lineWidth: configuration.lineWidth,
-            state: configuration.state,
-            color: { state in
-                switch state {
-                case .inProgress: .blue
-                case .failed: .red
-                case .succeeded: .green }
-            }
-        ) { progressState in
-            switch progressState {
-            case .inProgress: EmptyView()
-            case .failed: ExclamationView(lineWidth: configuration.lineWidth).foregroundColor(.red)
-            case .succeeded: CheckmarkView(lineWidth: configuration.lineWidth).foregroundColor(.green) }
-        }
-    }
+/// Allows to use the style as `.progressStyle(.rotating)` instead of `.progressStyle(RotatingCircularProgressStyle())``
+extension CircularProgressStyle where Self == RotatingProgressStyle {
+    public static var rotating: RotatingProgressStyle { RotatingProgressStyle() }
 }
